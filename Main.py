657ff737e039a7e6ec9958db734df67758eb0a87
Vasilev Bogdan <kot.begem0t.3.32.58@gmail.com>
2025-08-08 08:06:32 +0300

    update 08/08/2025

--- Main.py
+++ Main.py
@@ -1,11 +1,11 @@
-# Получить картинки кошек по API с сайта cataas.com с текстом
-#     Нужен текст для картинки
-#     Поэтому тексту можно найти/получить картинку
-#     Загрузить искомую картинку на Я.диск по "воздуху"
-#         Загружаем в папку на Я.диск, в папку с названием нашей группы
+# получить картинки кошек по API с сайта cataas.com с текстом
+#     нужен текст для картинки
+#     поэтому тексту можно найти/получить картинку
+#     загрузить искомую картинку на Я.диск по "воздуху"
+#         загружаем в папку на Я.диск, в папку с названием нашей группы
 #             если нет папки, нужно создать папку, присвоить папке имя моей группы
-#         Картинка должна иметь наименование такое же, как текст по которому мы её нашли
-#     После загрузки нужен файл с отчетом
+#         картинка должна иметь наименование такое же, как текст по которому мы её нашли
+#     после загрузки нужен файл с отчетом
 #         файл с информацией о размере файла картинки в json-файл
 #         файл поместить в папку report репозитории
 #             если нет папки, нужно создать папку, присвоить папке имя report
@@ -13,7 +13,7 @@
 import requests
 import Yandex_token
 
-text = 'hi'
+text = 'hello'
 
 class DownloadsImage:
     yd_url = 'https://cloud-api.yandex.net/'
@@ -29,7 +29,6 @@ class DownloadsImage:
     def create_yd_folder(self):
         url = 'v1/disk/resources'
         responses = requests.put(f'{self.yd_url}{url}?path={self.name_group}', headers=self.headers)
-        print(responses)
 
 
 class DownloadsImageCat(DownloadsImage):
@@ -40,9 +39,10 @@ class DownloadsImageCat(DownloadsImage):
 
     def download_cat_for_yd(self):
         url = 'v1/disk/resources/upload'
-        name = f'{self.text}_{requests.get('https://cataas.com/cat/says/').headers['Date']}'
         responses = requests.post(f'{self.yd_url}{url}?path={self.name_group}%2F{self.text}&url={self.cat_url}{self.text}', headers=self.headers)
-        print(responses)
+        if str(responses) == '<Response [409]>':
+            self.create_yd_folder()
+            return self.download_cat_for_yd()
 
 
 class DownloadsImageDog(DownloadsImage):
